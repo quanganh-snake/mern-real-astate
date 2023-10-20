c3a25cf7f2ad601e07717fcfd5f4f7696cafe921
quanganh-uneti <tbqanh@uneti.edu.vn>
2023-10-20 15:12:30 +0700

    create get user listings api route

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { helloUserRoute, updateUser, deleteUser } from "../controllers/user.controller.js";
+import { helloUserRoute, updateUser, deleteUser, getUserListing } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 const router = express.Router();
 
 router.get("/", helloUserRoute);
 router.post("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
+router.get("/listings/:id", verifyToken, getUserListing);
 
 export default router;
