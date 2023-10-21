b1d491df77f4b8f37cbd536d8b525a09066a4c8c
quanganh-uneti <tbqanh@uneti.edu.vn>
2023-10-21 08:29:51 +0700

    create update listing api route

--- backend/routes/listing.route.js
+++ backend/routes/listing.route.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { createListing, delteteListing } from "../controllers/listing.controller.js";
+import { createListing, delteteListing, updateListing } from "../controllers/listing.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.post("/create", verifyToken, createListing);
 router.delete("/delete/:id", verifyToken, delteteListing);
+router.post("/update/:id", verifyToken, updateListing);
 
 export default router;
